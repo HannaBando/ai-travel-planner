3dbd714cf452357f8ff3f44277f8e6fcf939b3aa
HannaBando <bando.hanna@gmail.com>
2026-05-09 00:18:18 +0200

    very general look of app

--- app.py
+++ app.py
@@ -1,1 +1,9 @@
-print("hello world")
+import streamlit as st
+
+st.title("Wanderly")
+st.subheader("Let AI generate your travel plan!")
+st.caption("Provide us with  your destination, budget, how many days and your interest and your whole concerns will disapear instantly")
+
+city = st.text_input("City")
+days = st.selectbox("Amount of days", ['1','2','3','4','5'])
+interests = st.multiselect('Interests', ['Musuems', 'Churches','Pubs','Cafes','Local Cusine', 'Beaches', 'Viewpoints'])
